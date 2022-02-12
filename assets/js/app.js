0ca5d7ef4d145caceb75280f9d47846bf19afbf8
kirega <joseph.mutiga@gmail.com>
2022-02-12 18:59:17 +0300

    Add moment tim conversion

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,6 +25,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import moment from "moment"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
@@ -42,3 +43,14 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
+
+var elements = document.getElementsByClassName("time_hour");
+for (each of elements) {
+  var t = each.innerText
+  var h = moment(t).hour()
+  if (h >= 12) {
+    each.innerText = `${moment(t).hour()} PM`
+  } else {
+    each.innerText = `${moment(t).hour()} AM`
+  }
+}
